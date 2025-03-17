08501684f9424b824a0762739b2a26eef8015215
Josh Lawman <jlawman@gmail.com>
2025-03-17 10:54:33 +0000

    Rerrange and cleanup

--- dev/vercel commands.md
+++
@@ -1,8 +0,0 @@
-# Link to project
-vercel link
-
-# Add env
-vercel env add [name] production < .env.local
-
-# List env
-vercel env ls
--- frontend/src/lib/braintrustlogger.ts
+++
@@ -1,6 +0,0 @@
-import { initLogger } from "braintrust";
-
-export const logger = initLogger({
-  projectName: "Red Panda Simple",
-  apiKey: process.env.BRAINTRUST_API_KEY,
-});
